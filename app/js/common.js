780fbb041c06161bba87471cae5469ac44902572
BlackStar1991 <zirkaandry@ukr.net>
2017-06-02 22:30:55 +0300

    clear slider

--- app/js/common.js
+++ app/js/common.js
@@ -3,9 +3,12 @@ window.onload = function () {
 
     var slider = document.getElementsByClassName("sliderBlock_items");
     var slides = document.getElementsByClassName("sliderBlock_items__itemPhoto");
-    var next = document.getElementsByClassName("sliderBlock_controls__arrowForward")[0]; // getElementsByClassName возвращает массив
+    var next = document.getElementsByClassName("sliderBlock_controls__arrowForward")[0]; // getElementsByClassName возвращает массив!
     var previous = document.getElementsByClassName("sliderBlock_controls__arrowBackward")[0];
     var items = document.getElementsByClassName("sliderBlock_positionControls")[0];
+
+
+
 
     var currentSlide = 0;
     var slideInterval = setInterval(nextSlide,5000);
@@ -30,7 +33,6 @@ window.onload = function () {
 
 
     next.onclick = function() {
-        console.log(slides.length);
         nextSlide();
     };
     previous.onclick = function() {
@@ -38,5 +40,11 @@ window.onload = function () {
     };
 
 
+
+    var currentSlideItem = document.getElementsByClassName("sliderBlock_positionControls__paginatorItem");
+
+    currentSlideItem.onclick =function () {
+        console.log(this.length);
+    }
 
 };
