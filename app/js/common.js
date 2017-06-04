5a92179c144f177417ef9096897a60beade36f35
BlackStar1991 <zirkaandry@ukr.net>
2017-06-04 21:35:01 +0300

    finish slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,50 +1,88 @@
 window.onload = function () {
 
+    var $ = document.querySelectorAll;
+
+
+   //// SLIDER
 
     var slider = document.getElementsByClassName("sliderBlock_items");
     var slides = document.getElementsByClassName("sliderBlock_items__itemPhoto");
     var next = document.getElementsByClassName("sliderBlock_controls__arrowForward")[0]; // getElementsByClassName возвращает массив!
     var previous = document.getElementsByClassName("sliderBlock_controls__arrowBackward")[0];
     var items = document.getElementsByClassName("sliderBlock_positionControls")[0];
-
-
-
+    var currentSlideItem = document.getElementsByClassName("sliderBlock_positionControls__paginatorItem");
 
     var currentSlide = 0;
-    var slideInterval = setInterval(nextSlide,5000);
+    var slideInterval = setInterval(nextSlide, 5000);
 
     function nextSlide() {
-        goToSlide( currentSlide + 1);
+        goToSlide(currentSlide + 1);
     }
 
-    function previousSlide(){
-        goToSlide( currentSlide - 1);
+    function previousSlide() {
+        goToSlide(currentSlide - 1);
     }
 
 
     function goToSlide(n) {
-         slides[currentSlide].className = 'sliderBlock_items__itemPhoto';
-         items.children[currentSlide].className = 'sliderBlock_positionControls__paginatorItem';
-         currentSlide = (n + slides.length)%slides.length;
-         slides[currentSlide].className = 'sliderBlock_items__itemPhoto sliderBlock_items__showing';
-         items.children[currentSlide].className = 'sliderBlock_positionControls__paginatorItem sliderBlock_positionControls__active';
+        slides[currentSlide].className = 'sliderBlock_items__itemPhoto';
+        items.children[currentSlide].className = 'sliderBlock_positionControls__paginatorItem';
+        currentSlide = (n + slides.length) % slides.length;
+        slides[currentSlide].className = 'sliderBlock_items__itemPhoto sliderBlock_items__showing';
+        items.children[currentSlide].className = 'sliderBlock_positionControls__paginatorItem sliderBlock_positionControls__active';
     }
 
 
-
-    next.onclick = function() {
+    next.onclick = function () {
         nextSlide();
     };
-    previous.onclick = function() {
+    previous.onclick = function () {
         previousSlide();
     };
 
 
+   
+    function goToSlideAfterPushTheMiniBlock() {
+        for (var i = 0; i < currentSlideItem.length; i++) {
+            currentSlideItem[i].onclick = function (i) {
+                var index = Array.prototype.indexOf.call(currentSlideItem, this);
+                goToSlide(index);
+            }
+        }
+    }goToSlideAfterPushTheMiniBlock();
 
-    var currentSlideItem = document.getElementsByClassName("sliderBlock_positionControls__paginatorItem");
 
-    currentSlideItem.onclick =function () {
-        console.log(this.length);
+/////////////////////////////////////////////////////////
+
+///// Specification Field
+
+
+    var buttonFullSpecification = document.getElementsByClassName("block_specification")[0];
+    var buttonSpecification = document.getElementsByClassName("block_specification__specificationShow")[0];
+    var buttonInformation = document.getElementsByClassName("block_specification__informationShow")[0];
+
+    var blockCharacteristiic = document.querySelector(".block_descriptionCharacteristic");
+    var activeCharacteristic  = document.querySelector(".block_descriptionCharacteristic__active");
+
+
+    buttonFullSpecification.onclick = function () {
+
+        console.log("OK");
+
+
+        buttonSpecification.classList.toggle("hide");
+        buttonInformation.classList.toggle("hide");
+
+
+        blockCharacteristiic.classList.toggle("block_descriptionCharacteristic__active");
+        blockCharacteristiic.classList.toggle("block_descriptionCharacteristic__disActive");
+
+
+
+
     }
+
+    
+
 
 };
